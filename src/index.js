a1d26eed12726f749a01719be663e2a542a84afb
duyhungphung <phungduyhung1992@gmail.com>
2024-02-20 19:34:43 +0700

    First commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,40 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom";
+import {PersistGate} from "redux-persist/integration/react";
+import storageSession from 'redux-persist/lib/storage/session';
+import {combineReducers, createStore} from "redux";
+import authReducer from "./reducers/authReducer";
+import {persistReducer, persistStore} from "redux-persist";
+import {Provider} from "react-redux";
+
+//redux
+const persistConFig = {
+    key : 'root',
+    storage: storageSession
+}
+
+const rootReducer = combineReducers({
+    auth: authReducer()
+    });
+
+const persistedReducer = persistReducer(persistConFig, rootReducer);
+
+const store = createStore(persistedReducer);
+
+const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+              <BrowserRouter>
+                  <App />
+              </BrowserRouter>
+          </PersistGate>
+      </Provider>
   </React.StrictMode>
 );
 
